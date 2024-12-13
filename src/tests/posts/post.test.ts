a8621351958b7a82afd1403d63d5564f901369f4
Idan17 <idanbartov@gmail.com>
2024-12-13 14:32:57 +0200

    feat: posts tests

--- src/tests/posts/post.test.ts
+++ src/tests/posts/post.test.ts
@@ -39,55 +39,55 @@ describe("Posts Test", () => {
     expect(response.body.content).toBe(posts[0].content);
     expect(response.body.userId).toBe(userId);
   });
-  // test("Get All Posts", async () => {
-  //   const response = await request(app)
-  //     .get("/posts/")
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.length).toBe(1);
-  // });
-  // test("Get Post by ID", async () => {
-  //   const response = await request(app)
-  //     .get(`/posts/id/${postId}`)
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.title).toBe(posts[0].title);
-  //   expect(response.body.content).toBe(posts[0].content);
-  //   expect(response.body.userId).toBe(userId);
-  // });
-  // test("Update Post", async () => {
-  //   const response = await request(app)
-  //     .put(`/posts/${postId}`)
-  //     .send({ title: "New Title" })
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.title).toBe("New Title");
-  // });
-  // test("Get All Posts by User ID", async () => {
-  //   const response = await request(app)
-  //     .get(`/posts/userId/${userId}`)
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.length).toBe(1);
-  // });
-  // test("Get All Posts by Username", async () => {
-  //   const response = await request(app)
-  //     .get(`/posts/username/${userTest.username}`)
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.length).toBe(1);
-  // });
-  // test("Delete Post", async () => {
-  //   const response = await request(app)
-  //     .delete(`/posts/${postId}`)
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  // });
-  // test("Get All Posts after Delete", async () => {
-  //   const response = await request(app)
-  //     .get("/posts/")
-  //     .set("Authorization", `Bearer ${accessToken}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body.length).toBe(0);
-  // });
+  test("Get All Posts", async () => {
+    const response = await request(app)
+      .get("/posts/")
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+  });
+  test("Get Post by ID", async () => {
+    const response = await request(app)
+      .get(`/posts/id/${postId}`)
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.title).toBe(posts[0].title);
+    expect(response.body.content).toBe(posts[0].content);
+    expect(response.body.userId).toBe(userId);
+  });
+  test("Update Post", async () => {
+    const response = await request(app)
+      .put(`/posts/${postId}`)
+      .send({ title: "New Title" })
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.title).toBe("New Title");
+  });
+  test("Get All Posts by User ID", async () => {
+    const response = await request(app)
+      .get(`/posts/userId/${userId}`)
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+  });
+  test("Get All Posts by Username", async () => {
+    const response = await request(app)
+      .get(`/posts/username/${userTest.username}`)
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+  });
+  test("Delete Post", async () => {
+    const response = await request(app)
+      .delete(`/posts/${postId}`)
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+  });
+  test("Get All Posts after Delete", async () => {
+    const response = await request(app)
+      .get("/posts/")
+      .set("Authorization", `Bearer ${accessToken}`);
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(0);
+  });
 });
