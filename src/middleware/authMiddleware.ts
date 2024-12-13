5b38460d624feda8a2923df07f80de74a7c96ea4
Hila <hila.ohana1910@gmail.com>
2024-12-13 13:14:12 +0200

    create auth middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyAccessToken } from "../utils/jwtUtils";
+
+export const authenticateJWT = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(403).json({ message: "Access denied" });
+  }
+
+  try {
+    const decoded = verifyAccessToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
