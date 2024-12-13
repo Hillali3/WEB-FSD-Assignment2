b5b8579f1754782cb7f4ae41b8965593bcfe282f
Idan17 <idanbartov@gmail.com>
2024-12-13 14:01:39 +0200

    feat: add posts test data

---
+++ src/tests/posts/postsTestsData.ts
@@ -0,0 +1,13 @@
+export const user = {
+  email: "bla",
+  password: "pass",
+  name: "hila",
+  username: "hila.ohana",
+};
+
+export const posts = [
+  {
+    title: "Post 1",
+    content: "Content 1",
+  },
+];
